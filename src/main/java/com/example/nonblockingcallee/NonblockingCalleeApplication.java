5b2193b88b0b1b897c1c79e27fd0054e16b24f3b
reboot0501 <add0602@gmail.com>
2021-12-28 22:09:53 +0900

    주석 추가

--- src/main/java/com/example/nonblockingcallee/NonblockingCalleeApplication.java
+++ src/main/java/com/example/nonblockingcallee/NonblockingCalleeApplication.java
@@ -1,3 +1,7 @@
+/**
+ * Spring boot MVC Remote Service
+ * Tomcat Base 구동
+ */
 package com.example.nonblockingcallee;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +17,21 @@ public class NonblockingCalleeApplication {
 	@RestController
 	public static class MyController {
 
+		/**
+		 * 1 초 소요 서비스
+		 */
 		@GetMapping("/service1")
 		public String service1(String req) throws InterruptedException {
-			Thread.sleep(1000);
+			Thread.sleep(1000); // 1초 대기
 			return req + " / service1";
 		}
+
+		/**
+		 * 1 초 소요 서비스
+		 */
 		@GetMapping("/service2")
 		public String service2(String req) throws InterruptedException {
-			Thread.sleep(1000);
+			Thread.sleep(1000); // 1초 대기
 			return req + " / service2";
 		}
 
